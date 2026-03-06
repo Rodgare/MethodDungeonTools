09e41931514908854ea8b89327b58e9f218d16a4
Alex <axel19911@gmail.com>
2026-03-07 00:51:32 +0200

    add hallsoflightning

---
+++ HallsOfLightning.lua
@@ -0,0 +1,9 @@
+local dungeonIndex = 2
+local nerfMultiplier = 1
+local pi = math.pi
+
+MethodDungeonTools.dungeonTotalCount[dungeonIndex] = { normal = 100, teeming = 100, teemingEnabled = false }
+
+MethodDungeonTools.dungeonBosses[dungeonIndex] = {}
+
+MethodDungeonTools.dungeonEnemies[dungeonIndex] = {}
